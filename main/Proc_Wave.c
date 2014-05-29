45ec8266ee2d7a95c0cb38f0ef091fa9e22fcf25
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-05-29 15:10:09 +0900

    D-5
    v-1.3
    ---
    1. option -dst
    
    	==> done

--- main/Proc_Wave.c
+++ main/Proc_Wave.c
@@ -61,19 +61,33 @@ char *file_dst_wave;
 /////////////////////////////////////
 void _Setup_Options_ProcWave(int argc, char **argv);
 void _Setup_Options_ProcWave__Src(char **argv);
+void _Setup_Options_ProcWave__Dst(char **argv);
 
+void Proc_Wave__SaveWave(void);
+void Proc_Wave__ReadWave(void);
+
+/////////////////////////////////////
+
+// Proc_Wave()
+
+/////////////////////////////////////
 void Proc_Wave(int argc, char **argv)
 {
 //	MONO_PCM pcm0, pcm1;
-	int n;
+//	int n;
 
 	/*********************************
 	 * Options
 	**********************************/
 	_Setup_Options_ProcWave(argc, argv);
 
+	/*********************************
+	 * Read: wave
+	**********************************/
+	Proc_Wave__ReadWave();
 
-//	mono_wave_read(&pcm0, "./audio/a.wav"); /* WAVEファイルからモノラルの音データを入力する */
+//	mono_wave_read(&pcm0, file_src_wave); /* WAVEファイルからモノラルの音データを入力する */
+////	mono_wave_read(&pcm0, "./audio/a.wav"); /* WAVEファイルからモノラルの音データを入力する */
 //	//  mono_wave_read(&pcm0, ".\\audio\\a.wav"); /* WAVEファイルからモノラルの音データを入力する */
 //	//  mono_wave_read(&pcm0, "..\\audio\\a.wav"); /* WAVEファイルからモノラルの音データを入力する */
 //	//  mono_wave_read(&pcm0, "a.wav"); /* WAVEファイルからモノラルの音データを入力する */
@@ -87,30 +101,52 @@ void Proc_Wave(int argc, char **argv)
 //	{
 //		pcm1.s[n] = pcm0.s[n]; /* 音データのコピー */
 //	}
+
+	/*********************************
+	 * Save: wav file
+	**********************************/
+	Proc_Wave__SaveWave();
+
+//	char delim = '.';
 //
-//	/*********************************
-//	 * Save: wav file
-//	**********************************/
+//	int position = 1; int num_of_tokens;
+//
+//	char **tokens = (char **) str_split_r_2
+//			(file_dst_wave, delim, position, &num_of_tokens);
+//
 //	int tmp_i = 4;
 //
 //	char **tmp = (char **) malloc(sizeof(char *) * tmp_i);
 //
-//	tmp[0] = "./audio/";
-//	tmp[1] = "b_";
+//	tmp[0] = tokens[0];
+//	tmp[1] = "_";
 //	tmp[2] = get_TimeLabel_Now(SERIAL);
 //	tmp[3] = ".wav";
+////	tmp[0] = "./audio/";
+////	tmp[1] = "b_";
+////	tmp[2] = get_TimeLabel_Now(SERIAL);
+////	tmp[3] = ".wav";
 //
 //	char *file_dst = join_simple(tmp, tmp_i);
 //
-//	mono_wave_write(&pcm1, file_dst); /* WAVEファイルにモノラルの音データを出力する */
+//	free(file_dst_wave);
+//
+//	file_dst_wave = file_dst;
+//
+//	mono_wave_write(&pcm1, file_dst_wave); /* WAVEファイルにモノラルの音データを出力する */
+////	mono_wave_write(&pcm1, file_dst_wave); /* WAVEファイルにモノラルの音データを出力する */
+////	mono_wave_write(&pcm1, file_dst); /* WAVEファイルにモノラルの音データを出力する */
 ////	mono_wave_write(&pcm1, "b.wav"); /* WAVEファイルにモノラルの音データを出力する */
 //
 //	//log
 //	printf("[%s : %d] wave => written: %s\n", base_name(__FILE__), __LINE__, file_dst);
-//
-//
-//	free(pcm0.s); /* メモリの解放 */
-//	free(pcm1.s); /* メモリの解放 */
+
+
+	/*********************************
+	 * Free: pcms
+	**********************************/
+	free(pcm0.s); /* メモリの解放 */
+	free(pcm1.s); /* メモリの解放 */
 //
 
 	//log
@@ -118,7 +154,7 @@ void Proc_Wave(int argc, char **argv)
 				base_name(__FILE__), __LINE__);
 
 
-}
+}//void Proc_Wave(int argc, char **argv)
 
 void _Setup_Options_ProcWave(int argc, char **argv)
 {
@@ -126,6 +162,11 @@ void _Setup_Options_ProcWave(int argc, char **argv)
 	 * Opt: size
 	**********************************/
 	_Setup_Options_ProcWave__Src(argv);
+
+	/*********************************
+	 * Opt: dst
+	**********************************/
+	_Setup_Options_ProcWave__Dst(argv);
 
 //	/*********************************
 //	 * Opt: bright
@@ -137,11 +178,6 @@ void _Setup_Options_ProcWave(int argc, char **argv)
 //	**********************************/
 //	_Setup_Options_ProcWave__Bg(argv);
 //
-//	/*********************************
-//	 * Opt: dst
-//	**********************************/
-//	_Setup_Options_ProcWave__Src(argv);
-
 
 }
 
@@ -151,6 +187,24 @@ void _Setup_Options_ProcWave__Src(char **argv) {
 
 	file_src_wave = (char *) get_Opt_Value(argv, opt_key_Src);
 
+	/*********************************
+	 * validate: exists?
+	**********************************/
+	int res_i = fileExists(file_src_wave);
+
+	if (res_i == false) {
+
+		consolColor_Change(RED);
+		//log
+		printf("[%s : %d] Src file => deosn't exist(%s)\n",
+				(char *) base_name(__FILE__), __LINE__, file_src_wave);
+
+		consolColor_Reset();
+
+		exit(-1);
+
+	}
+
 	consolColor_Change(GREEN);
 	//log
 	printf("[%s : %d] file_src_wave => %s\n",
@@ -159,3 +213,115 @@ void _Setup_Options_ProcWave__Src(char **argv) {
 	consolColor_Reset();
 
 }//void _Setup_Options_ProcWave__Src(char **argv)
+
+void _Setup_Options_ProcWave__Dst(char **argv) {
+
+	char *opt_key_Dst = "-dst";
+
+	file_dst_wave = (char *) get_Opt_Value(argv, opt_key_Dst);
+
+//	char delim = '.';
+//
+//	int position = 1; int num_of_tokens;
+//
+//	char **tokens = (char **) str_split_r_2
+//			(file_dst_wave, delim, position, &num_of_tokens);
+
+	//log
+	printf("[%s : %d] dir name => %s\n",
+			base_name(__FILE__), __LINE__, dir_name(file_dst_wave));
+
+	/*********************************
+	 * validate: folder exists?
+	**********************************/
+	char *dname = dir_name(file_dst_wave);
+
+	if (dname != NULL) {
+
+		int res_i = dirExists(dname);
+
+		if (res_i != 1) {
+
+			consolColor_Change(RED);
+
+			//log
+			printf("[%s : %d] Dst dir => doesn't exist: %s\n",
+						base_name(__FILE__), __LINE__, dname);
+
+			consolColor_Reset();
+
+			exit(-1);
+
+		}
+
+	}
+
+
+	consolColor_Change(GREEN);
+	//log
+	printf("[%s : %d] file_dst_wave => %s\n",
+			(char *) base_name(__FILE__), __LINE__, file_dst_wave);
+
+	consolColor_Reset();
+
+}//void _Setup_Options_ProcWave__Dst(char **argv)
+
+void Proc_Wave__SaveWave()
+{
+	char delim = '.';
+
+	int position = 1; int num_of_tokens;
+
+	char **tokens = (char **) str_split_r_2
+			(file_dst_wave, delim, position, &num_of_tokens);
+
+	int tmp_i = 4;
+
+	char **tmp = (char **) malloc(sizeof(char *) * tmp_i);
+
+	tmp[0] = tokens[0];
+	tmp[1] = "_";
+	tmp[2] = get_TimeLabel_Now(SERIAL);
+	tmp[3] = ".wav";
+//	tmp[0] = "./audio/";
+//	tmp[1] = "b_";
+//	tmp[2] = get_TimeLabel_Now(SERIAL);
+//	tmp[3] = ".wav";
+
+	char *file_dst = join_simple(tmp, tmp_i);
+
+	free(file_dst_wave);
+
+	file_dst_wave = file_dst;
+
+	mono_wave_write(&pcm1, file_dst_wave); /* WAVEファイルにモノラルの音データを出力する */
+//	mono_wave_write(&pcm1, file_dst_wave); /* WAVEファイルにモノラルの音データを出力する */
+//	mono_wave_write(&pcm1, file_dst); /* WAVEファイルにモノラルの音データを出力する */
+//	mono_wave_write(&pcm1, "b.wav"); /* WAVEファイルにモノラルの音データを出力する */
+
+	//log
+	printf("[%s : %d] wave => written: %s\n", base_name(__FILE__), __LINE__, file_dst);
+
+}
+
+void Proc_Wave__ReadWave()
+{
+	int n;
+
+	mono_wave_read(&pcm0, file_src_wave); /* WAVEファイルからモノラルの音データを入力する */
+//	mono_wave_read(&pcm0, "./audio/a.wav"); /* WAVEファイルからモノラルの音データを入力する */
+	//  mono_wave_read(&pcm0, ".\\audio\\a.wav"); /* WAVEファイルからモノラルの音データを入力する */
+	//  mono_wave_read(&pcm0, "..\\audio\\a.wav"); /* WAVEファイルからモノラルの音データを入力する */
+	//  mono_wave_read(&pcm0, "a.wav"); /* WAVEファイルからモノラルの音データを入力する */
+
+	pcm1.fs = pcm0.fs; /* 標本化周波数 */
+	pcm1.bits = pcm0.bits; /* 量子化精度 */
+	pcm1.length = pcm0.length; /* 音データの長さ */
+	pcm1.s = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
+
+	for (n = 0; n < pcm1.length; n++)
+	{
+		pcm1.s[n] = pcm0.s[n]; /* 音データのコピー */
+	}
+
+}
